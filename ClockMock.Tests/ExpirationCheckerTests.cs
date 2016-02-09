d27ea5751d7bef90648fc574ae9b54a256cceae6
Brent Sullivan <brntsllvn@gmail.com>
2016-02-09 15:57:51 -0800

    init

---
+++ ClockMock.Tests/ExpirationCheckerTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Channels;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace ClockMock.Tests
+{
+    [TestFixture]
+    class ExpirationCheckerTests
+    {
+        [Test]
+        public void HasExpiredReturnsNotNull()
+        {
+            // Arrange
+            var subject = new ExpirationChecker();
+
+            // Act
+            var result = subject.HasExpired();
+
+            // Assert
+            Assert.That(result, Is.Not.Null);
+        }
+
+    }
+}
---
+++ ClockMock/ExpirationChecker.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ClockMock
+{
+    public class ExpirationChecker
+    {
+        public bool HasExpired()
+        {
+            return false;
+        }
+    }
+}
